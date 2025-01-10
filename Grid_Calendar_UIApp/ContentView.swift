8174a2915dfa385654d821cdb1e5742ef7407ea6
adamcaak <ladamczak1997@gmail.com>
2025-01-10 17:51:30 +0100

    New stuct in file

--- Grid_Calendar_UIApp/ContentView.swift
+++ Grid_Calendar_UIApp/ContentView.swift
@@ -17,6 +17,42 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    struct Day: Identifiable {
+        let id = UUID()
+        let value: Int
+    }
+    
+    struct Month {
+        let name: String
+        let valueOfDay: Int
+        var days: [Day]
+        
+        init(name: String, valueOfDay: Int, days: [Day]) {
+            self.name = name
+            self.valueOfDay = valueOfDay
+            self.days = days
+            
+            for n in 1...valueOfDay {
+                self.days.append(Day(value: n))
+            }
+        }
+    }
+    
+    let year = [
+        Month(name: "January", valueOfDay: 31, days: []),
+        Month(name: "February", valueOfDay: 28, days: []),
+        Month(name: "March", valueOfDay: 31, days: []),
+        Month(name: "April", valueOfDay: 30, days: []),
+        Month(name: "May", valueOfDay: 31, days: []),
+        Month(name: "June", valueOfDay: 30, days: []),
+        Month(name: "July", valueOfDay: 31, days: []),
+        Month(name: "August", valueOfDay: 31, days: []),
+        Month(name: "September", valueOfDay: 30, days: []),
+        Month(name: "October", valueOfDay: 31, days: []),
+        Month(name: "November", valueOfDay: 30, days: []),
+        Month(name: "December", valueOfDay: 31, days: [])
+    ]
 }
 
 #Preview {
