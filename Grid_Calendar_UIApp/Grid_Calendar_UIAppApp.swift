bb15634257cb78a6ba6e72cff65ffad2bc0a4aeb
adamcaak <ladamczak1997@gmail.com>
2025-01-10 17:40:04 +0100

    Initial Commit

---
+++ Grid_Calendar_UIApp/Grid_Calendar_UIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_Calendar_UIAppApp.swift
+//  Grid_Calendar_UIApp
+//
+//  Created by Łukasz Adamczak on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Grid_Calendar_UIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
